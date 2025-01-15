3194823c81444a7801740e5b50dee278bbb9f8ce
junkonno <177442957+junkonno@users.noreply.github.com>
2025-01-16 07:27:52 +0900

    Add router

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -2,9 +2,17 @@ import {
     View, Text, TextInput, StyleSheet, TouchableOpacity
 } from 'react-native'
 
+import { Link, router } from 'expo-router'
 
 import Header from '../../components/Header'
 import Button from '../../components/Button'
+
+const handlePress = (): void => {
+    //login
+
+    //
+    router.push('/memo/list')
+}
 
 const LogIn = (): JSX.Element => {
     return(
@@ -14,12 +22,14 @@ const LogIn = (): JSX.Element => {
                 <Text style={styles.title}>Log In</Text>
                 <TextInput style={styles.input} value='Email address' />
                 <TextInput style={styles.input} value='Password' />
-                <Button label='Submit' />
+                <Button label='Submit' onPress={handlePress}/>
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Not registered?</Text>
-                    <TouchableOpacity>
-                        <Text style={styles.footerLink}>Sign up here!</Text>
-                    </TouchableOpacity>
+                    <Link href='/auth/sign_up' asChild>
+                        <TouchableOpacity>
+                            <Text style={styles.footerLink}>Sign up here!</Text>
+                        </TouchableOpacity>
+                    </Link>
                 </View>
             </View>
         </View>
